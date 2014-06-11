2e4f526d04300285fdf04a8b953d7386244bf41b
Helen Hood <helen.c.hood@gmail.com>
2014-06-11 11:43:24 -0400

    Complete articles show action & comments create

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
+require 'pry'
 
 # require all of the models and view templates
 Dir['app/**/*.rb'].each { |file| require_relative file }
@@ -11,4 +12,22 @@ set :views, 'app/views'
 get '/articles' do
   @articles = Article.order('created_at DESC')
   erb :'articles/index'
+end
+
+get '/articles/:id' do
+  @article = Article.find(params[:id])
+  erb :'articles/show'
+end
+
+post '/articles/:id/comments' do
+  @article = Article.find(params[:id])
+  @comment = Comment.new(body: params[:body],
+    author: params[:author], article: @article)
+
+  if @comment.save
+    redirect "/articles/#{@article.id}"
+  else
+    flash.now[:notice] = "Uh oh!  Could not post your comment."
+    erb :'articles/show'
+  end
 end

153bc8cde286551f9651e9c0df173595b54a6fb3
Helen Hood <helen.c.hood@gmail.com>
2014-06-11 07:57:31 -0400

    Add Categorization model & table, associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :categorizations
+  has_many :categories, through: :categorizations
 
   validates :title, presence: true
   validates :body, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+  has_many :categorizations
+  has_many :articles, through: :categorizations
+
   validates :name,
     presence: true,
     uniqueness: true
